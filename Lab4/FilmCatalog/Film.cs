6c6de04de73ac545dcfdbc8b2c44d85c6a7fe02c
Fleych <nikita17770@gmail.com>
2025-05-11 20:47:35 +0700

    Add files via upload

---
+++ Lab4/FilmCatalog/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCatalog
+{
+    internal class Film
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string genre { get; set; }
+        public string description { get; set; }
+        public Image image { get; set; }
+        
+        public Film(int id, string name, string genre, string description, Image image)
+        {
+            this.id = id;
+            this.name = name;
+            this.genre = genre;
+            this.description = description;
+            this.image = image;
+        }
+    }
+}
---
+++ Lab4/FilmCatalog/IdentityMap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace FilmCatalog
+{
+    internal class IdentityMap
+    {
+        private SQLiteConnection connection;
+        private Dictionary<int, Film> films = new Dictionary<int, Film>();
+
+        public IdentityMap(SQLiteConnection c)
+        {
+            connection = c;
+        }
+
+        public Film Get(int id)
+        {
+            films.TryGetValue(id, out Film film);
+            if (film != null)
+            {
+                return film;
+            }
+            using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM films WHERE id = @id", connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Image image = Image.FromFile(reader.GetString(4));
+                        film = new Film(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), image);
+                        films.Add(id, film);
+                        return film;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
